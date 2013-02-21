83947a946202c89226fb24a1630a7386852c159d
wenjun1055 <wenjun1055@gmail.com>
2013-02-21 21:41:37 +0800

    project init

---
+++ config.m4
@@ -0,0 +1,63 @@
+dnl $Id$
+dnl config.m4 for extension yal
+
+dnl Comments in this file start with the string 'dnl'.
+dnl Remove where necessary. This file will not work
+dnl without editing.
+
+dnl If your extension references something external, use with:
+
+dnl PHP_ARG_WITH(yal, for yal support,
+dnl Make sure that the comment is aligned:
+dnl [  --with-yal             Include yal support])
+
+dnl Otherwise use enable:
+
+dnl PHP_ARG_ENABLE(yal, whether to enable yal support,
+dnl Make sure that the comment is aligned:
+dnl [  --enable-yal           Enable yal support])
+
+if test "$PHP_YAL" != "no"; then
+  dnl Write more examples of tests here...
+
+  dnl # --with-yal -> check with-path
+  dnl SEARCH_PATH="/usr/local /usr"     # you might want to change this
+  dnl SEARCH_FOR="/include/yal.h"  # you most likely want to change this
+  dnl if test -r $PHP_YAL/$SEARCH_FOR; then # path given as parameter
+  dnl   YAL_DIR=$PHP_YAL
+  dnl else # search default path list
+  dnl   AC_MSG_CHECKING([for yal files in default path])
+  dnl   for i in $SEARCH_PATH ; do
+  dnl     if test -r $i/$SEARCH_FOR; then
+  dnl       YAL_DIR=$i
+  dnl       AC_MSG_RESULT(found in $i)
+  dnl     fi
+  dnl   done
+  dnl fi
+  dnl
+  dnl if test -z "$YAL_DIR"; then
+  dnl   AC_MSG_RESULT([not found])
+  dnl   AC_MSG_ERROR([Please reinstall the yal distribution])
+  dnl fi
+
+  dnl # --with-yal -> add include path
+  dnl PHP_ADD_INCLUDE($YAL_DIR/include)
+
+  dnl # --with-yal -> check for lib and symbol presence
+  dnl LIBNAME=yal # you may want to change this
+  dnl LIBSYMBOL=yal # you most likely want to change this 
+
+  dnl PHP_CHECK_LIBRARY($LIBNAME,$LIBSYMBOL,
+  dnl [
+  dnl   PHP_ADD_LIBRARY_WITH_PATH($LIBNAME, $YAL_DIR/lib, YAL_SHARED_LIBADD)
+  dnl   AC_DEFINE(HAVE_YALLIB,1,[ ])
+  dnl ],[
+  dnl   AC_MSG_ERROR([wrong yal lib version or lib not found])
+  dnl ],[
+  dnl   -L$YAL_DIR/lib -lm
+  dnl ])
+  dnl
+  dnl PHP_SUBST(YAL_SHARED_LIBADD)
+
+  PHP_NEW_EXTENSION(yal, yal.c, $ext_shared)
+fi
---
+++ php_yal.h
@@ -0,0 +1,83 @@
+/*
+  +----------------------------------------------------------------------+
+  | PHP Version 5                                                        |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1997-2012 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author:                                                              |
+  +----------------------------------------------------------------------+
+*/
+
+/* $Id: header 321634 2012-01-01 13:15:04Z felipe $ */
+
+#ifndef PHP_YAL_H
+#define PHP_YAL_H
+
+extern zend_module_entry yal_module_entry;
+#define phpext_yal_ptr &yal_module_entry
+
+#ifdef PHP_WIN32
+#	define PHP_YAL_API __declspec(dllexport)
+#elif defined(__GNUC__) && __GNUC__ >= 4
+#	define PHP_YAL_API __attribute__ ((visibility("default")))
+#else
+#	define PHP_YAL_API
+#endif
+
+#ifdef ZTS
+#include "TSRM.h"
+#endif
+
+PHP_MINIT_FUNCTION(yal);
+PHP_MSHUTDOWN_FUNCTION(yal);
+PHP_RINIT_FUNCTION(yal);
+PHP_RSHUTDOWN_FUNCTION(yal);
+PHP_MINFO_FUNCTION(yal);
+
+PHP_FUNCTION(confirm_yal_compiled);	/* For testing, remove later. */
+
+/* 
+  	Declare any global variables you may need between the BEGIN
+	and END macros here:     
+
+ZEND_BEGIN_MODULE_GLOBALS(yal)
+	long  global_value;
+	char *global_string;
+ZEND_END_MODULE_GLOBALS(yal)
+*/
+
+/* In every utility function you add that needs to use variables 
+   in php_yal_globals, call TSRMLS_FETCH(); after declaring other 
+   variables used by that function, or better yet, pass in TSRMLS_CC
+   after the last function argument and declare your utility function
+   with TSRMLS_DC after the last declared argument.  Always refer to
+   the globals in your function as YAL_G(variable).  You are 
+   encouraged to rename these macros something shorter, see
+   examples in any other php module directory.
+*/
+
+#ifdef ZTS
+#define YAL_G(v) TSRMG(yal_globals_id, zend_yal_globals *, v)
+#else
+#define YAL_G(v) (yal_globals.v)
+#endif
+
+#endif	/* PHP_YAL_H */
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
