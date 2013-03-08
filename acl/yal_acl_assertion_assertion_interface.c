76cffba15cde5f768b48bb9ab1547ea28df8988e
WenJun <wenjun1055@gmail.com>
2013-03-08 15:22:55 +0800

    add Yal\Acl\Assertion\AssertionInterface

---
+++ acl/yal_acl_assertion_assertion_interface.c
@@ -0,0 +1,56 @@
+/*
+  +----------------------------------------------------------------------+
+  | Yet Another Framework                                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: Jun Wen  <wenjun1055@gmail.com>                              |
+  +----------------------------------------------------------------------+
+*/
+  
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include "php.h"
+#include "php_yal.h"
+#include "yal_acl_assertion_assertion_interface.h"
+
+zend_class_entry * yal_acl_assertion_assertion_interface_ce;
+
+/** {{{ ARG_INFO
+*/
+ZEND_BEGIN_ARG_INFO_EX(yal_acl_assertion_assertion_interface_construct_arg, 0, 0, 1)
+    ZEND_ARG_OBJ_INFO(0, acl,       Yal\\Acl\\Acl, 0)
+    ZEND_ARG_OBJ_INFO(0, role,      Yal\\Acl\\Role\\RoleInterface, 0)
+    ZEND_ARG_OBJ_INFO(0, resource,  Yal\\Acl\\Resource\\ResourceInterface, 0)
+    ZEND_ARG_INFO(0, privilege)
+ZEND_END_ARG_INFO()
+/* }}} */
+
+/** {{{ yal_acl_role_interface_methods
+ */
+zend_function_entry yal_acl_assertion_assertion_interface_methods[] = {
+    ZEND_ABSTRACT_ME(yal_acl_assertion_assertion_interface, assert, yal_acl_assertion_assertion_interface_construct_arg)
+    {NULL, NULL, NULL}
+};
+/* }}} */
+
+/* {{{ ZEND_MINIT_FUNCTION
+ */
+ZEND_MINIT_FUNCTION(yal_acl_assertion_assertion_interface)
+{
+    zend_class_entry ce;
+    INIT_CLASS_ENTRY(ce, "Yal\\Acl\\Assertion\\AssertionInterface", yal_acl_assertion_assertion_interface_methods);
+    yal_acl_assertion_assertion_interface_ce = zend_register_internal_class_ex(&ce, NULL, NULL TSRMLS_CC);
+    yal_acl_assertion_assertion_interface_ce->ce_flags |= ZEND_ACC_INTERFACE;
+
+    return SUCCESS;
+}
+/* }}} */
---
+++ acl/yal_acl_assertion_assertion_interface.h
@@ -0,0 +1,23 @@
+/*
+  +----------------------------------------------------------------------+
+  | Yet Another Framework                                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: Jun Wen  <wenjun1055@gmail.com>                              |
+  +----------------------------------------------------------------------+
+*/
+  
+#ifndef YAL_ACL_ASSERTION_ASSERTION_INTERFACE_H
+#define YAL_ACL_ASSERTION_ASSERTION_INTERFACE_H
+
+extern zend_class_entry *yal_acl_assertion_assertion_interface_ce;
+
+ZEND_MINIT_FUNCTION(yal_acl_assertion_assertion_interface);
+#endif
